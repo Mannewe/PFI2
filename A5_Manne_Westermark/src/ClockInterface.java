95436b76753513a01b16e6212b88d40d385ed208
Manne Westermark <mannewestermark@gmail.com>
2015-02-18 10:08:49 +0100

    Början uppgift 5

---
+++ A5_Manne_Westermark/src/ClockInterface.java
@@ -0,0 +1,8 @@
+
+public interface ClockInterface {
+
+	public void update(int hours, int minute, int second);
+	
+	
+
+}
---
+++ A5_Manne_Westermark/src/ClockThread.java
@@ -0,0 +1,13 @@
+
+public class ClockThread extends Thread{
+private ClockInterface clockInterface;
+
+public ClockThread(ClockInterface clockInterface){
+	this.clockInterface = clockInterface;
+}
+
+public void run(){
+
+}
+
+}
